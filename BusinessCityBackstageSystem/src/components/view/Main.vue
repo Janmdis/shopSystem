b0e8b914b84953b0e0341fff0e6b0b748ec59197
samChen <293599505@qq.com>
2018-01-09 15:27:58 +0800

    addreset

--- BusinessCityBackstageSystem/src/components/view/Main.vue
+++ BusinessCityBackstageSystem/src/components/view/Main.vue
@@ -16,7 +16,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="less">
 .mains{
   background:yellow;
   width:100%;
