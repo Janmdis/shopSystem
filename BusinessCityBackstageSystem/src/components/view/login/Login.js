27f68ad4c3470016995a451e75b1637f0f88f673
szjSmiling <15839181512@163.com>
2018-01-10 18:07:10 +0800

    ...

--- BusinessCityBackstageSystem/src/components/view/login/Login.js
+++ BusinessCityBackstageSystem/src/components/view/login/Login.js
@@ -1,7 +1,83 @@
+/* eslint-disable */
 export default {
     data() {
         return {
-
+            ruleForm: {
+                userName: '',
+                userPwd: '',
+                checked: ''
+            },
         }
+    },
+    methods: {
+        checkName: function() {
+            console.log(this.ruleForm.username)
+        },
+        checkPwd: function() {
+            console.log(222)
+        },
+        logining(ruleForm) {
+            var name = this.ruleForm.userName; // 保存的账号
+            var pass = this.ruleForm.userPwd; // 保存的密码
+            if (name == '' || name == null) {
+                alert("请输入正确的用户名");
+                return
+            } else if (pass == '' || pass == null) {
+                alert("请输入正确的密码");
+                return
+            }
+            //判断复选框是否被勾选 勾选则调用配置cookie方法
+            if (this.checked = true) {
+                //传入账号名，密码，和保存天数3个参数
+                this.setCookie(name, pass, 7);
+            }
+            //接口
+            var url = 'myserver/user/login';
+            this.$http.post(url, this.ruleForm, { emulateJSON: true })
+                .then(res => {
+                    if (res.body == "fail") {
+                        alert("用户名或密码错误,请重新输入");
+                        this.ruleForm.userName = '';
+                        this.ruleForm.password = '';
+                        return
+                    } else {
+                        this.$alert('3秒后自动跳转到...', '登陆成功', {
+                            confirmButtonText: '确定',
+                        });
+                        //this.$router.push("/index")
+                    }
+                });
+        },
+        //设置cookie
+        setCookie(c_name, c_pwd, exdays) {
+            var exdate = new Date(); //获取时间
+            exdate.setTime(exdate.getTime() + 24 * 60 * 60 * 1000 * exdays); //保存的天数
+            //字符串拼接cookie
+            window.document.cookie = "userName" + "=" + c_name + ";path=/;expires=" + exdate.toGMTString();
+            window.document.cookie = "userPwd" + "=" + c_pwd + ";path=/;expires=" + exdate.toGMTString();
+        },
+        //读取cookie
+        getCookie: function() {
+            if (document.cookie.length > 0) {
+                var arr = document.cookie.split('; '); //这里显示的格式需要切割一下自己可输出看下
+                for (var i = 0; i < arr.length; i++) {
+                    var arr2 = arr[i].split('='); //再次切割
+                    //判断查找相对应的值
+                    if (arr2[0] == 'userName') {
+                        this.ruleForm.userName = arr2[1]; //保存到保存数据的地方
+                    } else if (arr2[0] == 'userPwd') {
+                        this.ruleForm.password = arr2[1];
+                    }
+                }
+            }
+        },
+        //清除cookie
+        clearCookie: function() {
+            this.setCookie("", "", -1); //修改2值都为空，天数为负1天就好了
+        }
+    },
+    //页面加载调用获取cookie值
+    mounted() {
+        this.getCookie()
     }
 }
--- BusinessCityBackstageSystem/src/components/view/login/Login.vue
+++ BusinessCityBackstageSystem/src/components/view/login/Login.vue
@@ -5,24 +5,22 @@
             <div class="contentRight">
                 <h3>欢迎进入禾目云视觉健康管理系统</h3>
                 <h5> Background management system </h5>
-                <form action="" class="loginForm">
+                <el-form :model="ruleForm" action="" class="loginForm">
                     <ul class="loginUl">
-                        <li><label><img src="/static/images/Login/userman.png" alt=""><input type="text" value="" placeholder="请输入登录账号或手机号" ></label></li>
-                        <li><label><img src="/static/images/Login/regpass.png" alt=""><input type="password" value="" placeholder="请输入登录密码" ></label></li>
-                        <li class="liFr">
-                            <div id="checkCode"></div>
-                            <span>验证码:</span><input type="text" id="codeInput" placeholder="请输入验证码" >
-                        </li>
-                        <li class="liTh"><label><input type="checkbox" name="remPassword" id="" value=""><span class="liThSpan">记住密码</span></label><span>忘记密码</span></li>
-                        <li class="liFo"><span id="myButton">立即登录</span></li>
+                        <li> <label><img src="/static/images/Login/userman.png" alt=""><el-input type="text" @change="checkName" value="" v-model="ruleForm.userName" placeholder="请输入登录账号" clearable></el-input></label></li>
+                        <li><label><img src="/static/images/Login/regpass.png" alt=""><el-input type="password" @change="checkPwd" value="" v-model="ruleForm.userPwd" placeholder="请输入登录密码" clearable></el-input></label></li>
+                        <li class="liFr"><div id="checkCode"></div><span>验证码:</span><input type="text" id="codeInput" placeholder="请输入验证码" ></li>
+                        <li class="liTh"><label><el-checkbox v-model="ruleForm.checked" name="remPassword" @click="clearCookie" >记住密码</el-checkbox></label><span>忘记密码?</span></li>
+                        <li class="liFo"><span id="myButton" @click="logining(ruleForm)" >立即登录</span></li>
                     </ul>
-                </form>
+                </el-form>
             </div>
         </el-col>
     </el-row>
   </div>
 </template>
 <script>
+/* eslint-disable */
 import logins from './Login';
 export default logins
 // var verifyCode = new GVerify("checkCode");
@@ -35,7 +33,31 @@ export default logins
 // 	}
 // }
 </script>
+
 <style scoped lang='less'>
 @import "./Login.less";
+</style>
+<style>
+.el-input{
+    display:inline-block;
+    width:70%;
+    background:transparent;
+}
+.el-input .el-input__inner{
+    border:none;
+    vertical-align: 5px;
+    background:transparent;
+    padding-left:15px;
+}
+.el-input .el-input__suffix{
+    top:-5px;
+}
+.el-checkbox__label{
+    color:#fff;
+    margin-right: 50px;
+}
+.el-checkbox__input.is-checked+.el-checkbox__label{
+    color:#0f0;
+}   
 </style>
 
