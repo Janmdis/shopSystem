af6f5ca46c1f716c1fd06983d979b32dec858b35
szjSmiling <15839181512@163.com>
2018-01-11 16:56:30 +0800

    login

---
+++ BusinessCityBackstageSystem/src/components/view/member/memberInfo/memberInfo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="memberInfo">
+
+    </div>
+</template>
+<script>
+/* import memberInfos from './memberInfo.js';
+export default memberInfos; */
+export default{
+    data () {
+        return {
+            
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+@import "./MemberInfo.less";
+</style>
+<style>
+
+</style>
+
