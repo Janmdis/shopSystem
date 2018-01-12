89223a8ffd1bc7f3abb58a7e9dc4755aa0180ac1
szjSmiling <15839181512@163.com>
2018-01-12 16:49:21 +0800

    szj

---
+++ BusinessCityBackstageSystem/src/components/view/member/memberInfo/Order.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="order">
+        <h2>订单</h2>
+    </div>
+</template>
+<script>
+export default{
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
+<script scoped>
+
+</script>
---
+++ BusinessCityBackstageSystem/src/components/view/member/memberInfo/Report.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="report">
+        <h2>报告</h2>
+    </div>
+</template>
+<script>
+export default{
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
+<script scoped>
+
+</script>
