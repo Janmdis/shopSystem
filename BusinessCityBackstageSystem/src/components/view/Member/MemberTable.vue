e05c518a15c4908574472c966169d39343443b0b
samChen <293599505@qq.com>
2018-01-13 19:44:25 +0800

    Member代码更新

---
+++ BusinessCityBackstageSystem/src/components/view/Member/MemberTable.vue
@@ -0,0 +1,124 @@
+<template>
+    <el-table
+    :data="datalist"
+    @select='showextra(false)'
+    @select-all='showextra(true)'
+    :stripe='true'
+    style="width: 100%">
+    <el-table-column
+    fixed
+    type="index"
+    label="N"
+    :index="indexMethod">
+    </el-table-column>
+                <el-table-column
+                fixed
+                type="selection"
+                width="55">
+                </el-table-column>
+                <el-table-column
+                class='borderRight'
+                fixed
+                prop="ids"
+                label="ID"
+                width='100'>
+                </el-table-column>
+                <el-table-column
+                prop="name"
+                label="客户姓名">
+                </el-table-column>
+                <el-table-column
+                prop="iphone"
+                width='120'
+                label="手机号">
+                </el-table-column>
+                <el-table-column
+                prop="types"
+                label="客户类型">
+                </el-table-column>
+                <el-table-column
+                prop="city"
+                label="城市">
+                </el-table-column>
+                <el-table-column
+                prop="quarters"
+                label="小区">
+                </el-table-column>
+                <el-table-column
+                prop="state"
+                label="订单状态">
+                </el-table-column>
+                <el-table-column
+                prop="source"
+                label="来源">
+                </el-table-column>
+                <el-table-column
+                prop="Inputtiem"
+                width='100'
+                label="录入时间">
+                </el-table-column>
+                <el-table-column
+                width='260'
+                prop="address"
+                label="小区地址">
+                </el-table-column>
+                
+            </el-table>
+</template>
+<script>
+export default {
+    props:['data'],
+    data(){
+        return {
+            datalist:[]
+        }
+    },
+    created:function(){
+        this.datalist=this.data;
+    },
+    methods:{
+        showextra(isall){
+            let inputdom=document.getElementsByClassName('el-table__body-wrapper')[0].getElementsByTagName('input');
+            let num=0;
+            let allnum=inputdom.length;
+            for(let i=0;i<inputdom.length;i++){
+                if(inputdom[i].checked){
+                    num++;
+                }
+            }
+            let show=false;
+            let editcan=true;
+            if(isall){
+                if(num==0||num!=allnum){
+                    show=true;
+                    editcan=false;
+                    num=allnum;
+                }
+                else{
+                    show=false;
+                    num=0;
+                }
+            }
+            else{
+                if(num!=0){
+                    show=true;
+                    editcan=num>1?false:true;
+                }
+                else{
+                    show=false;
+                }
+            }
+            this.$root.$emit('showlttip',{show,editcan,num});
+            
+            // console.log(num);
+            // if((!isall&&num==allnum)||(isall&&num==0)){
+            //     console.log('全选');
+            // }
+        },
+        indexMethod(index) {
+        return index + 1
+        },
+    }
+
+}
+</script>
---
+++ BusinessCityBackstageSystem/src/components/view/Member/search.vue
@@ -0,0 +1,155 @@
+<template>
+    <div>
+    
+        <el-form ref="form" :model="form" label-width="80px">
+    
+            <el-row>
+    
+                <el-col :span="4">
+    
+                    <el-form-item label="手机号">
+    
+                        <el-input v-model="form.name"></el-input>
+    
+                    </el-form-item>
+    
+                </el-col>
+    
+                <el-col :span="4">
+    
+                    <el-form-item label="小区">
+    
+                        <el-input v-model="form.name"></el-input>
+    
+                    </el-form-item>
+    
+                </el-col>
+    
+                <el-col :span="4">
+    
+                    <el-form-item label="来源">
+    
+                        <el-select v-model="form.region" placeholder="请选择活动区域">
+    
+                            <el-option label="区域一" value="shanghai"></el-option>
+    
+                            <el-option label="区域二" value="beijing"></el-option>
+    
+                        </el-select>
+    
+                    </el-form-item>
+    
+                </el-col>
+    
+            </el-row>
+    
+            <el-row>
+    
+                <el-col :span="4">
+    
+                    <el-form-item label="订单状态">
+    
+                        <el-select v-model="form.region1" placeholder="请选择活动区域">
+    
+                            <el-option label="区域一" value="shanghai"></el-option>
+    
+                            <el-option label="区域二" value="beijing"></el-option>
+    
+                        </el-select>
+    
+                    </el-form-item>
+    
+                </el-col>
+    
+                <el-col :span="4">
+    
+                    <el-form-item label="客户类型">
+    
+                        <el-select v-model="form.region2" placeholder="请选择活动区域">
+    
+                            <el-option label="区域一" value="shanghai"></el-option>
+    
+                            <el-option label="区域二" value="beijing"></el-option>
+    
+                        </el-select>
+    
+                    </el-form-item>
+    
+                </el-col>
+    
+                <el-col :span="4">
+    
+                    <el-form-item label="回访记录">
+    
+                        <el-select v-model="form.region3" placeholder="请选择活动区域">
+    
+                            <el-option label="区域一" value="shanghai"></el-option>
+    
+                            <el-option label="区域二" value="beijing"></el-option>
+    
+                        </el-select>
+    
+                    </el-form-item>
+    
+                </el-col>
+    
+                <el-col :span="3" class="mLeft">
+    
+                    <el-button type="primary" round>查询</el-button>
+    
+                </el-col>
+    
+                <el-col :span="2">
+    
+                    <i class="el-icon-refresh"></i>
+    
+                </el-col>
+    
+            </el-row>
+    
+        </el-form>
+    
+    </div>
+</template>
+<style>
+    
+</style>
+<script>
+    export default {
+    
+        data() {
+    
+            return {
+    
+                form: {
+    
+                    name: '',
+    
+                    region: '',
+    
+                    region1: '',
+    
+                    region2: '',
+    
+                    region3: '',
+    
+                    date1: '',
+    
+                    date2: '',
+    
+                    delivery: false,
+    
+                    type: [],
+    
+                    resource: '',
+    
+                    desc: ''
+    
+                }
+    
+            }
+    
+        }
+    
+    }
+</script>
