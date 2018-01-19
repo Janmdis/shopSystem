1c9835b4d399e7edbabf97878d3c60dec74ff362
White.X <948169119@qq.com>
2018-01-19 17:23:44 +0800

    update

--- BusinessCityBackstageSystem/src/components/view/mall/customTemplate/templateSet.vue
+++ BusinessCityBackstageSystem/src/components/view/mall/customTemplate/templateSet.vue
@@ -26,7 +26,7 @@
                       <!-- banner -->
                       <banner></banner>
                       <keep-alive v-for='(item,index) in comlist' :key='index'>
-                        <components :templatedata='index' :is='item'  class="test" @click.native='changetop(index)'></components>
+                        <components :templatedata='index' :is='item'  class="test"  @click.native='changetop(index,item)'></components>
                       </keep-alive>
                       <!-- 图片广告组件 -->
                       <!-- <imageAds></imageAds> -->
@@ -85,7 +85,10 @@
                 <div class="arrow"></div>
                 <div>
                     <!-- banner/图片广告编辑组件 -->
-                    <imageAdEditing v-bind="imgdata"></imageAdEditing>
+                    <!-- <imageAdEditing v-bind="imgdata"></imageAdEditing> -->
+                    <keep-alive>
+                        <components :is='left' ></components>
+                    </keep-alive>
                     <!-- 橱窗编辑组件 -->
                     <!-- <windowEditing v-bind="windowdata"></windowEditing> -->
                     <!-- 橱窗2编辑组件 -->
@@ -162,6 +165,9 @@ import customLink from './customLink'
              imgdata:'',
              prodata:'',
              comlist:[],
+             left:'imageAdEditing',
+             leftlist:['productEditing','titlesEditing','imageAdEditing','classificationEditing','windowEditing','windowEditing2'],
+             list2:['commodity','titles','imageAds','classification','window','window2']
          }
      },
      created:function(){
@@ -196,10 +202,12 @@ import customLink from './customLink'
          classificationAdd(){
              this.comlist.push('classification');
         },
-        changetop(index){
+        changetop(index,item){
             let jump = document.querySelectorAll('.test');
             let top=jump[index].offsetTop-100;
             document.getElementsByClassName('template-editContent-div')[0].style.marginTop=top+'px';
+            let eq=this.list2.indexOf(item);
+            this.left=this.leftlist[eq];
         }
      },
      components:{
