cc003a3019b72fd4d51a7e3759af374306e56569
White.X <948169119@qq.com>
2018-03-13 15:23:04 +0800

    update

--- BusinessCityBackstageSystem/src/components/view/ProductOnline/package/detailpackage.vue
+++ BusinessCityBackstageSystem/src/components/view/ProductOnline/package/detailpackage.vue
@@ -484,7 +484,7 @@ export default {
         // 获取产品分类
         getcategory(){
             let that=this;
-            this.$http.post('/api/product/commodity/category/queryMap')
+            this.$http.post('/api/product/commodity/category/queryMap',{})
             .then(function(response){
                 if(response.data.msg=='查询成功'){
                     that.goodstype=response.data.info;
@@ -641,7 +641,7 @@ export default {
         // 获取时间模板
         getdatemodel(currentvalue){
             let that=this;
-            this.$http.post('/api/product/commodity/periodTemplate/queryMap')
+            this.$http.post('/api/product/commodity/periodTemplate/queryMap',{})
             .then(function(response){
                 if(response.data.msg=='查询成功'){
                     that.listmodaltime=response.data.info;
@@ -1052,7 +1052,7 @@ export default {
 // 地区模板处理
         getareamodel(){
             let that=this;
-            this.$http.post('/api/product/commodity/regionTemplate/queryMap')
+            this.$http.post('/api/product/commodity/regionTemplate/queryMap',{})
             .then(function(response){
                 if(response.data.msg=='查询成功'){
                     that.listmodalarea=response.data.info;
@@ -1101,7 +1101,7 @@ export default {
         // 查询所有城市
         getcitys(){
             let that=this;
-            this.$http.get('/api/public/region/findParent?parentId&levels=3')
+            this.$http.get('/api/public/region/findParent?parentId=0&levels=3',{})
             .then(function(response){
                 if(response.data.msg=='查询成功'){
                     let list=response.data.info;
